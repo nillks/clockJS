7085ef66231065614ba08e76b057dddb1efa92e4
nillks <daniil.troanov2@gmail..com>
2024-02-07 00:27:45 +0600

    feat: сделал функционал переключения стрелок

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-var фыв = 1;
-function ME() {
-  console.log(kek);
+const handSecond = document.querySelector(".time__second");
+const handMinute = document.querySelector(".time__minute");
+const handHour = document.querySelector(".time__hour");
+
+function setTime() {
+  const currentTime = {
+    second: new Date().getSeconds(),
+    minute: new Date().getMinutes(),
+    hour: new Date().getHours(),
+  };
+
+  handSecond.style.transform = `rotate(${
+    (currentTime.second / 60) * 360 + 90
+  }deg)`;
+  handMinute.style.transform = `rotate(${
+    (currentTime.minute / 60) * 360 + 90
+  }deg)`;
+  handHour.style.transform = `rotate(${(currentTime.hour / 60) * 360 + 90}deg)`;
 }
+
+setInterval(setTime, 1000);
